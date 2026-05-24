7ddc275edc76277e8cd7596a678b3bf8a5a9c349
thanasab <thanasab.li@gmail.com>
2026-05-24 20:56:11 +0700

    bump: sw.js cache v16 after encoding fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // WIBWUB Service Worker Ã¢ÂÂ auto-update on new version
-const CACHE = 'wibwub-v15';
+const CACHE = 'wibwub-v16';
 const FILES = [
   '/Wibwub-Dashboard/WIBWUB_Mobile.html',
   '/Wibwub-Dashboard/manifest.json',
